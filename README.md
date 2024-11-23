be084e4ae7cfcda8f68478c516f6999e59efa703
Ashen Jayarathne <ashenjay01@gmail.com>
2024-11-23 08:51:06 +0530

    Store variables using config package

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Different ways of working with environment specific variables in Node.js applications
+
+---
+
+<br />
+
+## By using "config" npm package
+
+- default.json is just a template file that store the structure of an actual env file
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+const config = require("config");
 const express = require("express");
 
 // Create express application
 const app = express();
+
+console.log(config.get("jwtPrivateKey"));
 
 // Start server
 const port = process.env.PORT || 5005;
